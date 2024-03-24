5d2c21d7b3e73e2b027418669ff5e62a7d10899b
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-03-24 23:30:55 +0530

    prepared header now moving on to body

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { FaBars } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className="flex justify-between">
+      <div className='flex items-center justify-center gap-2'>
+        <img src="./assets/Logo.svg" alt="logo" />
+        <button className='bg-gradient-to-r from-orange-400 to-red-400 text-xs text-white px-2.5 py-1.5 rounded-2xl'>Hosterr is hiring</button>
+
+      </div>
+      <div>
+        <FaBars />
+      </div>
     </div>
   );
 }
