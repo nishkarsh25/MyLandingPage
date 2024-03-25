fc3eb2b4d72acde3a26becd578948e01b56e4de9
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-03-25 13:07:48 +0530

    completed footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,10 +2,28 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className='flex flex-col gap-12'>
+      <ul className='flex gap-6 font-lato text-gray-400'>
+        <li>
+          <a href="#">Facebook</a>
+        </li>
+        <li>
+          <a href="#">Instagram</a>
+        </li>
+        <li>
+          <a href="#">Twitter</a>
+        </li>
+      </ul>
+      <div className='flex gap-2'>
+        <img src="public/assets/Help Avatar.svg" alt="help" />
+        <div>
+          <p className='font-playfair font-thin'>Have any questions?</p>
+          <a href="" className='font-lato font-medium'>Talk to a specialist</a>
+        </div>
+      </div>
     </div>
   );
 }
+
 
 export default Footer
