6dd2c8782e18b0c5df4858697f569fad87165ef4
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-03-25 13:58:05 +0530

    making the layout responsive for large screens say for laptops

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,7 +9,16 @@ const Header = () => {
         <button className='bg-gradient-to-r from-orange-400 to-red-400 text-xs text-white px-2.5 py-1.5 rounded-2xl'>Hosterr is hiring</button>
 
       </div>
-      <div>
+      <ul className='hidden lg:flex justify-between items-center font-lato text-gray-400 gap-6'>
+        <li><a href="#">Plans</a></li>
+        <li><a href="#">Find Domain</a></li>
+        <li><a href="#">Why Hoster</a></li>
+      </ul>
+      <div className='hidden lg:flex justify-center items-center font-lato gap-6'>
+        <a className='text-gray-400' href="#">Sign In</a>
+        <button className='rounded-md px-4 py-3 bg-blue-400 hover:bg-blue-600 text-white'>Join WaitList</button>
+      </div>
+      <div className='lg:hidden'>
         <FaBars />
       </div>
     </div>
