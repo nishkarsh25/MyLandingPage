b4bde5307cf46c0e9b0237779fbb21be466956c8
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-03-25 13:16:33 +0530

    making the layout responsive for different screen say for medium screens say ipad air(done for ipad)

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -4,15 +4,15 @@ const Body = () => {
   return (
     <div className='space-y-4'>
       <div className="flex items-center justify-center">
-        <img src="./assets/Blue Shape.svg" alt="1st" className='-rotate-45 h-64' />
-        <img src="./assets/Pink Shape.svg" alt="2nd" className='absolute -rotate-[30deg] h-64'/>
-        <img src="./assets/Purple Shape.svg" alt="3rd" className='absolute -rotate-[15deg] h-64' />
-        <img src="./assets/Hero Image (Model).png" alt="hero" className='absolute h-64' />
+        <img src="./assets/Blue Shape.svg" alt="1st" className='-rotate-45 h-64 md:h-72' />
+        <img src="./assets/Pink Shape.svg" alt="2nd" className='absolute -rotate-[30deg] h-64 md:h-72'/>
+        <img src="./assets/Purple Shape.svg" alt="3rd" className='absolute -rotate-[15deg] h-64 md:h-72' />
+        <img src="./assets/Hero Image (Model).png" alt="hero" className='absolute h-64 md:h-72' />
       </div>
       <div>
         <h1 className='text-5xl font-bold font-playfair leading-tight'>Host your website in less than 5 minutes</h1>
         <p className='font-lato text-gray-400'>With Hosterr, get your website up and running in no less than 5 minutes with the most competitive pricing packages available online.</p>
-        <form action="" className='flex flex-col gap-4'>
+        <form action="" className='flex flex-col gap-4 md:flex-row'>
           <input className='rounded-md px-4 py-3 placeholder:text-gray-400' type="email" placeholder='Enter email address' />
           <button className='rounded-md px-4 py-3 bg-blue-400 hover:bg-blue-600 text-white'>Join Waitlist</button>
         </form>
